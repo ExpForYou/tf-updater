9d76d985e64e0f9ae37c4cf62389ebda68011839
Shelby Skalnik <sskalnik@sskalnik.com>
2020-01-28 23:09:43 +0000

    Initial commit of working script

---
+++ get-latest-terraform.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# If there's a new Terraform releases available, delete the current Terraform install and download the new one.
+# Must be run from within the directory where terraform binaries should reside
+#
+
+LATEST_RELEASE_TAG=$(curl https://api.github.com/repos/hashicorp/terraform/releases/latest | jq --raw-output '.tag_name' | cut -c 2-)
+LATEST_RELEASE=$(awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }' <<< $LATEST_RELEASE_TAG)
+CURRENT_TF_VERSION=$(terraform -v | awk 'NR==1{print $NF}' | cut -c 2- | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }')
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  OS=linux
+else
+  OS=$OSTYPE
+fi
+
+if [ ${LATEST_RELEASE} -gt ${CURRENT_TF_VERSION} ]; then
+   echo "Installing Terraform ${LATEST_RELEASE_TAG} for ${OS}..."
+   cd /tmp/
+   wget https://releases.hashicorp.com/terraform/${LATEST_RELEASE_TAG}/terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip
+   sudo unzip terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip -d /usr/local/bin
+   rm terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip
+   cd -
+else
+   echo "Latest Terraform already installed."
+fi
