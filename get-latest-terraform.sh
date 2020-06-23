ac05dae1a55c2cd55bc7dde6ed9b9535595726ad
Shelby Skalnik <42625392+sskalnik@users.noreply.github.com>
2020-06-23 17:12:28 -0500

    Fix quotation typo in unzip command

--- get-latest-terraform.sh
+++ get-latest-terraform.sh
@@ -25,7 +25,7 @@ if [ "${LATEST_RELEASE}" -gt "${CURRENT_TF_VERSION}" ]; then
    echo "Installing Terraform ${LATEST_RELEASE_TAG} for ${OS}..."
    cd /tmp/ || exit
    wget -q "https://releases.hashicorp.com/terraform/${LATEST_RELEASE_TAG}/terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip"
-   unzip "terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip -d /usr/local/bin"
+   unzip "terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip" -d /usr/local/bin
    chmod +x /usr/local/bin/terraform
    rm "terraform_${LATEST_RELEASE_TAG}_${OS}_amd64.zip"
    cd -
